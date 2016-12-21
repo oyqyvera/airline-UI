a01ef1b25331485c900054b6d6fcfd9f72bc852b
Qianyu Ouyang <oyqy@qianyus-macbook-pro.local>
2016-12-21 12:11:57 -0500

    Initial Commit

---
+++ vehicle/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  vehicle
+//
+//  Created by Qianyu Ouyang on 12/21/16.
+//  Copyright © 2016 Qianyu Ouyang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
