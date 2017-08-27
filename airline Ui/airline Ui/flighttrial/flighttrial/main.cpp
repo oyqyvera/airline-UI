f32374e856babe4164b7bc632e1c62ef7ba0344f
oyqy <oyqy@Qianyus-MacBook-Pro.local>
2017-08-27 15:44:23 -0400

    first commit

---
+++ airline Ui/airline Ui/flighttrial/flighttrial/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  flighttrial
+//
+//  Created by Qianyu Ouyang on 5/9/17.
+//  Copyright © 2017 Qianyu Ouyang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
---
+++ airline Ui/airline Ui/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  airline Ui
+//
+//  Created by Qianyu Ouyang on 5/5/17.
+//  Copyright © 2017 Qianyu Ouyang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
